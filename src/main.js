fea4bdc544748d63fa1d48368a2fd742cada5507
Chekwube Ezechi <zechirayjr@yahoo.com>
2023-07-04 08:05:27 +0100

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import "../css/modern-normalize.css";
 import "../css/style.css";
 import "../css/components/header.css";
+import "../css/components/hero.css";
+import "../css/components/about.css";
+import "../css/components/work.css";
+import "../css/components/contact.css";
+import "../css/components/footer.css";
 import "../css/utils.css";
